fe8459fb2daed3e930d0aae433f9a968a99ef561
Daniel <dhonaruto@gmail.com>
2024-07-12 15:35:36 -0400

    update mongoose connection configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
@@ -7,9 +7,22 @@ import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string,{
+      tls: true,
+      tlsAllowInvalidCertificates: true, // Remove this in production
+    });
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
+};
 
 const app = express();
+
+// Middleware
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +35,7 @@ app.use(
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
