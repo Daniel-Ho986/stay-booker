daa7540682f076ce84c97202768c44a0281f3bfd
Daniel <dhonaruto@gmail.com>
2024-08-12 16:05:14 -0400

    added stripe & booking feature & e2e test

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -1,3 +1,11 @@
+export type UserType = {
+  _id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+};
+
 export type HotelType = {
   _id: string;
   userId: string;
@@ -13,6 +21,7 @@ export type HotelType = {
   starRating: number;
   imageUrls: string[];
   lastUpdated: Date;
+  bookings: BookingType[];
 };
 
 export type HotelSearchResponse = {
@@ -23,3 +32,22 @@ export type HotelSearchResponse = {
     pages: number;
   };
 };
+
+export type BookingType = {
+  _id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  adultCount: number;
+  childCount: number;
+  checkIn: Date;
+  checkOut: Date;
+  totalCost: number;
+}
+
+export type PaymentIntentResponse = {
+  paymentIntentId: string;
+  clientSecret: string;
+  totalCost: number;
+}
